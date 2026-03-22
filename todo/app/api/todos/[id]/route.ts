9bd428d63f9fbc7f2fbc5a6d073a6eb3fd3fe187
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-03-22 17:56:49 +0100

    fix params type error

--- todo/app/api/todos/[id]/route.ts
+++ todo/app/api/todos/[id]/route.ts
@@ -1,32 +1,31 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 import { db } from "@/lib/db/index"
 import { chores } from "@/lib/db/schema"
 import { eq } from "drizzle-orm"
 
 export async function PATCH(
-    request: Request,
-    { params }: { params: {id: string } }
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    const { done } = await request.json()
-    const id = Number(params.id)
+  const { id } = await params
+  const { done } = await request.json()
 
-    const [updated] = await db
+  const [updated] = await db
     .update(chores)
     .set({ done })
-    .where(eq(chores.id, id))
+    .where(eq(chores.id, Number(id)))
     .returning()
 
-    return NextResponse.json(updated)
+  return NextResponse.json(updated)
 }
 
 export async function DELETE(
-    _request: Request,
-    { params }: { params: {id: string} }
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    const id = Number(params.id)
+  const { id } = await params
 
-    await db.delete(chores).where(eq(chores.id, id))
+  await db.delete(chores).where(eq(chores.id, Number(id)))
 
-    return NextResponse.json({ success: true})
+  return NextResponse.json({ success: true })
 }
-    
