f4f192a8eec487b3021718709a6e0c8841aef98b
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-03-22 18:13:55 +0100

    fix revalidatePath on list creation

--- todo/app/api/lists/route.ts
+++ todo/app/api/lists/route.ts
@@ -1,22 +1,24 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 import { db } from "@/lib/db/index"
-import { listsTable } from "@/lib/db/schema";
-
+import { listsTable } from "@/lib/db/schema"
+import { revalidatePath } from "next/cache"
 
 export async function POST(request: Request) {
-    const { name } = await request.json()
+  const { name } = await request.json()
 
-    if(!name || typeof name !== "string" || name.trim() === "") {
-        return NextResponse.json(
-            { error: "Name is required"},
-            { status: 400}
-        )
-    }
+  if (!name || typeof name !== "string" || name.trim() === "") {
+    return NextResponse.json(
+      { error: "Name is required" },
+      { status: 400 }
+    )
+  }
 
-    const [newList] = await db
+  const [newList] = await db
     .insert(listsTable)
     .values({ name: name.trim() })
     .returning()
 
-    return NextResponse.json(newList, {status: 201})
+  revalidatePath("/")
+
+  return NextResponse.json(newList, { status: 201 })
 }
